fdd7a11e3c70da3c3ccc5fdf7bd40ee65f284815
kkk394 <2567069z@gsuite.kobe-u.ac.jp>
2026-01-14 14:36:09 +0900

    Add files via upload

--- number.py
+++ number.py
@@ -3,7 +3,7 @@ st.title("電卓")
 num1=st.number_input(input("1つ目の数字を入力してください"))
 num2=st.number_input(input("2つ目の数字を入力してください"))
 operation=st.selectbox("計算方法を選んでください",("足し算","引き算","掛け算","割り算"))
-if st.button("計算する")
+if st.button("計算する"):
     if operation=="足し算":
          st.write("結果:",num1+num2)
     elif operation=="引き算":
